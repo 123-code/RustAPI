61c3904aae5feb268900bce7e5e62297d9b6ab23
123-code <naranjojose256@gmail.com>
2023-12-15 16:17:27 -0500

    updates

--- rust_api/src/main.rs
+++ rust_api/src/main.rs
@@ -4,7 +4,8 @@ use std::net::TcpListener;
 
 #[actix_web::main]
 
+
 async fn main()-> std::io::Result<()>{
 let listener: TcpListener = TcpListener::bind("127.0.0.1:8000")?;
-run(listener)?.await 
+run(listener).await?.await 
 }
--- rust_api/src/server.rs
+++ rust_api/src/server.rs
@@ -1,10 +1,30 @@
 use actix_web::{
-    web,App,HttpRequest,HttpServer,HttpResponse
+    web, App, HttpRequest, HttpServer, HttpResponse
 };  
+
+use tokio_postgres::{NoTls, Error as PgError};
 
 use actix_web::dev::Server;
 
 use std::net::TcpListener;
+
+async fn handle_connection() -> Result<HttpResponse, PgError> {
+    let database_url = "";
+    let (client,connection) = tokio_postgres::connect(database_url,NoTls).await?;
+    
+    
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+
+    format!("Connected to db");
+
+
+    Ok(HttpResponse::Ok().finish())
+}
 
 
 async fn app_works()->HttpResponse{
@@ -13,9 +33,15 @@ async fn app_works()->HttpResponse{
 
   
 
-pub fn run(listener:TcpListener) -> Result<Server,std::io::Error>{
+pub async fn run(listener:TcpListener) -> Result<Server,std::io::Error>{
+    let result = handle_connection().await;
+
+
+    //println!("{:?}",result.unwrap());
     let server = HttpServer::new(||{
+    
     App::new()
+ 
     .route("/",web::get().to(app_works))
     })
     .listen(listener)?
