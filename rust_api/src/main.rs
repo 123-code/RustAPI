35a1d267e51aeabd9a97c17741658f247ccee295
123-code <jose.naranjo.martinez@udla.edu.ec>
2023-12-25 23:32:35 -0500

    _-_

--- rust_api/src/main.rs
+++ rust_api/src/main.rs
@@ -9,3 +9,4 @@ async fn main()-> std::io::Result<()>{
 let listener: TcpListener = TcpListener::bind("127.0.0.1:8000")?;
 run(listener).await?.await 
 }
+
--- rust_api/src/server.rs
+++ rust_api/src/server.rs
@@ -1,26 +1,47 @@
 use actix_web::{
     web, App, HttpRequest, HttpServer, HttpResponse
-};  
-
+}; 
+use serde::{Deserialize, Serialize}; 
 use tokio_postgres::{NoTls, Error as PgError};
-
 use actix_web::dev::Server;
-
 use std::net::TcpListener;
+use dotenv::dotenv;
+use std::env;
+
+
+[derive(Serialize, Deserialize)]
+
+struct ProposalData{
+    header:String,
+    description:String
+}
+
+async fn save_data(form:web::Form<ProposalData>)->HttpResponse{
+    let data = form.into_inner();
+    HttpResponse::Ok().json(data)
+    
+}
 
 async fn handle_connection() -> Result<HttpResponse, PgError> {
-    let database_url = "";
-    let (client,connection) = tokio_postgres::connect(database_url,NoTls).await?;
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set in .env");
+
+    let (client,connection) = tokio_postgres::connect(&database_url,NoTls).await?;
     
     
     tokio::spawn(async move {
         if let Err(e) = connection.await {
             eprintln!("connection error: {}", e);
         }
+        else {
+            println!("Connected to db");
+        }
+     
     });
 
 
-    format!("Connected to db");
+    
 
 
     Ok(HttpResponse::Ok().finish())
@@ -34,7 +55,7 @@ async fn app_works()->HttpResponse{
   
 
 pub async fn run(listener:TcpListener) -> Result<Server,std::io::Error>{
-    let result = handle_connection().await;
+    handle_connection().await;
 
 
     //println!("{:?}",result.unwrap());
